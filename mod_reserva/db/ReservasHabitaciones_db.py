a0209a69012cb98ce44d51b449713c4263187623
feljadue <feljadue@bancolombia.com.co>
2020-12-10 14:06:49 -0500

    Cambio estructura DB, se agrega tabla ReservasHabitaciones y se modifica Reservas

---
+++ mod_reserva/db/ReservasHabitaciones_db.py
@@ -0,0 +1,74 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Dict
+
+class ReservasHabitacionesDB(BaseModel):
+
+    id_reserva: int
+    id_habitacion: str
+    fecha_inicio: date
+    fecha_fin: date
+
+database_ReservaHabitaciones: Dict[int, ReservasHabitacionesDB]
+
+database_users = {
+    1: ReservasHabitacionesDB(**{"id_reserva":1,
+                       "habitacion":1,
+                       "fecha_inicio":"2020-10-15",
+                       "fecha_fin":"2020-10-20"
+                       }),
+    
+    2: ReservasHabitacionesDB(**{"id_reserva":1,
+                       "habitacion":4,
+                       "fecha_inicio":"2020-10-15",
+                       "fecha_fin":"2020-10-20"
+                       }),
+
+    3: ReservasHabitacionesDB(**{"id_reserva":1,
+                       "habitacion":5,
+                       "fecha_inicio":"2020-10-15",
+                       "fecha_fin":"2020-10-20"
+                       }),
+
+    4: ReservasHabitacionesDB(**{"id_reserva":2,
+                       "habitacion":3,
+                       "fecha_inicio":"2020-11-01",
+                       "fecha_fin":"2020-11-04"
+                       }),
+
+    5: ReservasHabitacionesDB(**{"id_reserva":3,
+                       "habitacion":1,
+                       "fecha_inicio":"2020-12-24",
+                       "fecha_fin":"2021-01-02"
+                       }),
+
+    6: ReservasHabitacionesDB(**{"id_reserva":3,
+                       "habitacion":2,
+                       "fecha_inicio":"2020-12-24",
+                       "fecha_fin":"2021-01-02"
+                       }),
+
+    7: ReservasHabitacionesDB(**{"id_reserva":4,
+                       "habitacion":2,
+                       "fecha_inicio":"2020-12-10",
+                       "fecha_fin":"2020-12-23"
+                       }),
+
+    8: ReservasHabitacionesDB(**{"id_reserva":5,
+                       "habitacion":3,
+                       "fecha_inicio":"2021-02-20",
+                       "fecha_fin":"2021-02-22"
+                       }),
+    
+    9: ReservasHabitacionesDB(**{"id_reserva":5,
+                       "habitacion":4,
+                       "fecha_inicio":"2021-02-20",
+                       "fecha_fin":"2021-02-22"
+                       }),
+
+    10: ReservasHabitacionesDB(**{"id_reserva":5,
+                       "habitacion":5,
+                       "fecha_inicio":"2021-02-20",
+                       "fecha_fin":"2021-02-22"
+                       })
+}
--- mod_reserva/db/Reservas_db.py
+++ mod_reserva/db/Reservas_db.py
@@ -5,9 +5,6 @@ from typing import Dict
 class ReservasDB(BaseModel):
 
     id_reserva: int
-    habitacion: str
-    fecha_inicio: date
-    fecha_fin: date
     id_quien_realiza: int
     nombre_cliente: str
     apellido_cliente: str
@@ -20,9 +17,6 @@ database_reservas: Dict[int, ReservasDB]
 
 database_users = {
     1: ReservasDB(**{"id_reserva":1,
-                       "habitacion":"1, 4, 5",
-                       "fecha_inicio":"2020-10-15",
-                       "fecha_fin":"2020-10-20",
                        "id_quien_realiza": 43578657,
                        "nombre_cliente": "Carlos",
                        "apellido_cliente": "Lopez",
@@ -33,9 +27,6 @@ database_users = {
                        }),
 
     2: ReservasDB(**{"id_reserva":2,
-                       "habitacion":"3",
-                       "fecha_inicio":"2020-11-01",
-                       "fecha_fin":"2020-11-04",
                        "id_quien_realiza": 103879657,
                        "nombre_cliente": "Andrea",
                        "apellido_cliente": "Hernandez",
@@ -46,9 +37,6 @@ database_users = {
                        }),
 
     3: ReservasDB(**{"id_reserva":3,
-                       "habitacion":"1, 2",
-                       "fecha_inicio":"2020-12-24",
-                       "fecha_fin":"2021-01-02",
                        "id_quien_realiza": 103879657,
                        "nombre_cliente": "Fernando",
                        "apellido_cliente": "Campos",
@@ -59,9 +47,6 @@ database_users = {
                        }),
 
     4: ReservasDB(**{"id_reserva":4,
-                       "habitacion":"2",
-                       "fecha_inicio":"2020-12-10",
-                       "fecha_fin":"2020-12-23",
                        "id_quien_realiza": 43578657,
                        "nombre_cliente": "Armando",
                        "apellido_cliente": "Corrales",
@@ -72,9 +57,6 @@ database_users = {
                        }),
 
     5: ReservasDB(**{"id_reserva":5,
-                       "habitacion":"3, 4, 5",
-                       "fecha_inicio":"2021-02-20",
-                       "fecha_fin":"2021-02-22",
                        "id_quien_realiza": 43578657,
                        "nombre_cliente": "Andres",
                        "apellido_cliente": "Ramos",
